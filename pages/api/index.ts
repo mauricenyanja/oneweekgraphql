9d0044611eed8643dbca0ae5db2b43a29f0b6df8
Nyash-Mauro <mauricee423@gmail.com>
2022-11-03 11:33:06 +0300

    created createcontext function tha returns a graphql from prisma

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,36 +1,47 @@
-import { PrismaClient } from '@prisma/client'
-
 import { createServer } from "@graphql-yoga/node";
 import { join } from "path";
-import { readFileSync } from "fs"
-import { Resolvers } from "../../types"
+import { readFileSync } from "fs";
+import { Resolvers } from "../../types";
+import type { PrismaClient } from "@prisma/client";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-import prisma from "../../lib/prisma"
-
-
-const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
-    encoding: "utf-8",
-})
-const resolvers:Resolvers = {
-    Query: {
-      cart: (_, { id }) => {
-        return {
-          id,
-          totalItems: 0,
-        };
-      },
-    },
-}
-const server = createServer({
-    cors: false,
-    endpoint: "/api"
-    ,
-    schema: {
-      typeDefs,
-      resolvers,
-    },
-  });
+import prisma from "../../lib/prisma";
 
 export type GraphQLContext = {
-    prisma: PrismaClient;
+  prisma: PrismaClient;
+};
+
+export async function createContext(): Promise<GraphQLContext> {
+  return {
+    prisma,
   };
+}
+
+const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
+  encoding: "utf-8",
+});
+
+const resolvers: Resolvers = {
+  Query: {
+    cart: (_, { id }) => {
+      return {
+        id,
+        totalItems: 0,
+      };
+    },
+  },
+};
+
+const server = createServer<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
+  endpoint: "/api",
+  schema: {
+    typeDefs,
+    resolvers,
+  },
+  context: createContext(),
+});
+
+export default server.requestListener;
