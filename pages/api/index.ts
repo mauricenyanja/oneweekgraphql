77dd99e2b377f0dda7ec15c4d9753c0b9b8989e5
Nyash-Mauro <mauricee423@gmail.com>
2022-11-04 09:49:54 +0300

    imported findorcreatecart from libs/cart and

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,15 +1,13 @@
 import { createServer } from "@graphql-yoga/node";
 import { join } from "path";
 import { readFileSync } from "fs";
-import { Resolvers } from "../../types";
+import currencyFormatter from "currency-formatter";
 import type { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "../../lib/prisma";
-
-import currencyFormatter from "currency-formatter";
-
-const currencyCode = "USD";
+import { Resolvers } from "../../types";
+import { findOrCreateCart } from "../../lib/cart";
 
 export type GraphQLContext = {
   prisma: PrismaClient;
@@ -21,6 +19,8 @@ export async function createContext(): Promise<GraphQLContext> {
   };
 }
 
+const currencyCode = "USD";
+
 const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
   encoding: "utf-8",
 });
@@ -28,27 +28,40 @@ const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
 const resolvers: Resolvers = {
   Query: {
     cart: async (_, { id }, { prisma }) => {
-      let cart = await prisma.cart.findUnique({
-        where: { id },
+      return findOrCreateCart(prisma, id);
+    },
+  },
+  Mutation: {
+    addItem: async (_, { input }, { prisma }) => {
+      const cart = await findOrCreateCart(prisma, input.cartId);
+      await prisma.cartItem.upsert({
+        create: {
+          cartId: cart.id,
+          id: input.id,
+          name: input.name,
+          description: input.description,
+          image: input.image,
+          price: input.price,
+          quantity: input.quantity || 1,
+        },
+        where: { id_cartId: { id: input.id, cartId: cart.id } },
+        update: {
+          quantity: {
+            increment: input.quantity || 1,
+          },
+        },
       });
-
-      if (!cart) {
-        cart = await prisma.cart.create({
-          data: { id },
-        });
-      }
-
       return cart;
     },
   },
   Cart: {
     items: async ({ id }, _, { prisma }) => {
+      // https://www.prisma.io/docs/guides/performance-and-optimization/query-optimization-performance#solving-n1-in-graphql-with-findunique-and-prismas-dataloader
       const items = await prisma.cart
         .findUnique({
           where: { id },
         })
         .items();
-
       return items;
     },
     totalItems: async ({ id }, _, { prisma }) => {
@@ -57,7 +70,6 @@ const resolvers: Resolvers = {
           where: { id },
         })
         .items();
-
       return items.reduce((total, item) => total + item.quantity || 1, 0);
     },
     subTotal: async ({ id }, _, { prisma }) => {
@@ -66,9 +78,28 @@ const resolvers: Resolvers = {
           where: { id },
         })
         .items();
-
       const amount =
         items.reduce((acc, item) => acc + item.price * item.quantity, 0) ?? 0;
+      return {
+        amount,
+        formatted: currencyFormatter.format(amount / 100, {
+          code: currencyCode,
+        }),
+      };
+    },
+  },
+  CartItem: {
+    unitTotal: (item) => {
+      const amount = item.price;
+      return {
+        amount,
+        formatted: currencyFormatter.format(amount / 100, {
+          code: currencyCode,
+        }),
+      };
+    },
+    lineTotal: (item) => {
+      const amount = item.quantity * item.price;
 
       return {
         amount,
@@ -80,10 +111,7 @@ const resolvers: Resolvers = {
   },
 };
 
-const server = createServer<{
-  req: NextApiRequest;
-  res: NextApiResponse;
-}>({
+const server = createServer({
   endpoint: "/api",
   schema: {
     typeDefs,
