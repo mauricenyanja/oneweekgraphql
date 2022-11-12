eeb1b096a09516221c374f71f08054435860bee3
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:18:44 +0300

    made imports to thankyou file

---
+++ pages/thankyou.tsx
@@ -0,0 +1,8 @@
+import { removeCookies } from "cookies-next"
+import { GetServerSideProps, NextPage } from "next"
+import Router from "next/router"
+import Stripe from "stripe"
+import { CartDetail } from "../components/CartDetail"
+import { Header } from "../components/Header"
+import { stripe } from "../lib/stripe"
+import { useGetCartQuery } from "../types"
