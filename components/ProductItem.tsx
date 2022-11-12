ba96158ffac0dd59adcc5d8cbcd70a131bd3040c
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 01:20:37 +0300

    exported func productItem

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,13 @@
 import Image from "next/image"
-import { HtmlProps } from "next/dist/shared/lib/html-context"
+import { HTMLProps } from "react"
 import { Product } from "../lib/product"
+
+export function ProductItem({
+    product: { price,src,title },
+}: HTMLProps<HTMLDivElement> & {
+    product: Product
+}){
+    return (
+        
+    )
+}
