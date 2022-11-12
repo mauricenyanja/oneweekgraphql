3ce293466fc3cc160ccf1dd76290c36e9858c5a7
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:55:20 +0300

    added an svg to closeIcon

--- components/CloseIcons.tsx
+++ components/CloseIcons.tsx
@@ -1,5 +1,11 @@
 export function CloseIcon(){
     return (
-        
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        />
     )
 }
