71b4de06994133ca9a2b94d5fc13cfe583d4beaf
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 01:14:44 +0300

    created add to cart html view

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -8,4 +8,40 @@ export function ProductDetail({ product }: { product:Product | null }) {
     const [addToCart, {loading}] = useAddToCartMutation({
         refetchQueries: [GetCartDocument],
     })
+    if (!product){
+        return null
+    }
+    return (
+        <main className="grid grid-cols-4 h-[700px]">
+        <div className="col-span-3 flex items-center justify-center">
+          <ProductItem product={product} />
+        </div>
+        <form
+          className="p-8 space-y-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addToCart({
+              variables: {
+                input: {
+                  cartId,
+                  id: product.id,
+                  name: product.title,
+                  description: product.body,
+                  price: product.price,
+                  image: product.src,
+                },
+              },
+            });
+          }}
+        >
+          <div dangerouslySetInnerHTML={{ __html: product.body }} />
+          <button
+            className="px-6 py-4 bg-black rounded w-full text-white hover:bg-white hover:text-black border border-black uppercase"
+            type="submit"
+          >
+            {loading ? "Adding to cart..." : "Add to cart"}
+          </button>
+        </form>
+      </main>
+    )
 }
