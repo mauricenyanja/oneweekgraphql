3a334556fa348c7d0b91b4f268661d1638634f1c
Nyash-Mauro <mauricee423@gmail.com>
2022-11-12 23:37:49 +0300

    exported useClient by using useMemoHook and inMemory cache to cache the apolloclient

--- lib/client.ts
+++ lib/client.ts
@@ -1,14 +1,25 @@
 import { useMemo } from "react"
-import {ApolloClient,InMemoryCahe} from "@apollo/ApolloClient"
+import { ApolloClient, InMemoryCache } from "@apollo/ApolloClient"
 
-const protocol = `${
-    process.env.NODE_ENV === "development" ? "http" : "https"
+const protocol = `${process.env.NODE_ENV === "development" ? "http" : "https"
   }://`;
-  
-  const host =
-    typeof window === "undefined"
-      ? process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"
-      : // Use host on the client since using VERCEL_URL can lead to CORS errors due to aliases
-        window.location.host;
-  
-  export const origin = `${protocol}${host}`;
+
+const host =
+  typeof window === "undefined"
+    ? process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"
+    : // Use host on the client since using VERCEL_URL can lead to CORS errors due to aliases
+    window.location.host;
+
+export const origin = `${protocol}${host}`
+
+export const useClient = () => {
+  const client = useMemo(
+    () =>
+      new ApolloClient({
+        uri: `${origin}/api`,
+        cache: new InMemoryCache(),
+      }),
+    []
+  )
+  return client;
+}
