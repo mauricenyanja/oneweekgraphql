2f528a9876d3adda0a5a9ec39ab7be4517b63ae4
Nyash-Mauro <mauricee423@gmail.com>
2022-11-12 23:45:38 +0300

    made imports to slug Header Products

---
+++ pages/products/[slug].tsx
@@ -0,0 +1,7 @@
+import { GetServerSideProps,InferGetServerSidePropsType,NextPage }
+ from "next"
+
+ import { Header } from "../../components/Header";
+ import { ProductDetail } from "../../components/ProductDetail";
+ import { Product, products } from "../../lib/products";
+ 
