7b0a049da60e438bc54d23394adc199b2e5e72ba
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:41:55 +0300

    created cartError function and exported it

--- components/CartError.tsx
+++ components/CartError.tsx
@@ -3,3 +3,8 @@ import { removeCookies } from "cookies-next"
 import { ExclamationCircle } from "./ExclamationCircle"
 
 
+export function CartError({ error }: { error:Error | undefined }){
+    if (!error){
+        return null
+    }
+}
