2f371b6ebcf3183b27a964a5b648bab97010eac7
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 01:30:14 +0300

    exported funct ProductLInk

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -3,5 +3,20 @@ import { Product } from "../lib/product"
 import { ProductItem } from "./ProductItem"
 
 export function ProductList({ products }: { products:Product[] }){
-    
+    return (
+        <ul className="grid grid-flow-row-dense grid-cols-1 md:grid-cols-2">
+            {products.slice(0,6)[Symbol].map((product,index)=>(
+                <ProductLink key={index} product={product}/>
+            ))}
+        </ul>
+    )
+}
+export function ProductLink({ product }: { product:Product }){
+    return(
+        <Link href={`/products/${product.slug}`} key={product.slug}>
+        <a style={{ height: 500 }} className="bg-gray-400">
+          <ProductItem product={product} />
+        </a>
+      </Link>
+    )
 }
