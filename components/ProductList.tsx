9b2555a6c4aaac3323fc7a612f2ec22d1efb5882
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 01:25:52 +0300

    exported productslist

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,3 +1,7 @@
 import Link from "next/link"
 import { Product } from "../lib/product"
 import { ProductItem } from "./ProductItem"
+
+export function ProductList({ products }: { products:Product[] }){
+    
+}
