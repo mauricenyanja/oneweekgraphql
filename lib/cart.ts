ed73e37455cb0e45260690cc216f3892b2e2d5a8
Nyash-Mauro <mauricee423@gmail.com>
2022-11-12 23:30:58 +0300

    added export to validateCartItems and added push to checkout item data

--- lib/cart.ts
+++ lib/cart.ts
@@ -21,5 +21,25 @@ export function validateCartItems(
   inventory: Product[],
   CartItems: CartItem[]
 ): Stripe.Checkout.SessionCreateParams.LineItem[] {
-  
+  const checkoutItems: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+ for (const item of cartItems){
+  const product = inventory.find(({id}) => id === item.id)
+  if (!product){
+    throw new Error(`Item with id ${item.id} is not in the inventory`)
+  }
+  checkoutItems.push({
+    quantity: item.quantity,
+    price_data: {
+      currency: "USD",
+      unit_amount: product.price,
+      product_data:{
+        name:item.name,
+        description:item.description || undefined,
+        images : item.image ? [item.image] : [],
+      }
+    }
+  })
+ } 
+ return checkoutItems
 }
