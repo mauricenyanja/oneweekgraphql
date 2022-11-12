e63fec90915d1c78f88ac57f8b46bd8cf0ac085e
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:13:43 +0300

    exported getstatic props in index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,14 @@ import { Header } from "../components/Header"
 import { ProductList } from "../components/ProductList"
 import { Product, products } from "../lib/products"
 
+
+export const GetStaticProps:GetStaticProps<{
+  products: Product[]
+}> = async () => ({
+  props: {
+    products:products.slice(0,6)
+  }
+})
 const Home: NextPage = () => {
   return <h1 className="text-purple-500 text-xl">Hello world</h1>;
 };
