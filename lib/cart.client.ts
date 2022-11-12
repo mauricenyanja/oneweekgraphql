7b9d7db11a84553ebd339b6d701eed68ff9c336a
Nyash-Mauro <mauricee423@gmail.com>
2022-11-12 23:11:41 +0300

    added export func to useCart

--- lib/cart.client.ts
+++ lib/cart.client.ts
@@ -3,3 +3,10 @@ import { IncomingMessage, ServerResponse } from "http";
 import { NextApiRequestCookies } from "next/dist/server/api-utils"
 import { v4 as uuid } from "uuid"
 import { useGetCartQuery } from "../types"
+
+export function useCart() {
+    const cartId = String(getCookie("cartId"));
+    const { data } = useGetCartQuery({ variables: { id: cartId } });
+    return data?.cart;
+  }
+  
