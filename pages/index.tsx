04787f08975c57394ba04f3c0ab98480ca6ff4fc
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:11:18 +0300

    made imports for getting props and compontes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-import type { NextPage } from "next";
+import type {GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { Header } from "../components/Header"
+import { ProductList } from "../components/ProductList"
+import { Product, products } from "../lib/products"
 
 const Home: NextPage = () => {
   return <h1 className="text-purple-500 text-xl">Hello world</h1>;
