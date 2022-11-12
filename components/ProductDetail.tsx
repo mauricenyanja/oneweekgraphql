803a7b88dab422ed4677d7081f57909d7375b9f5
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 01:08:47 +0300

    imported in productDetails file

---
+++ components/ProductDetail.tsx
@@ -0,0 +1,4 @@
+import { getCookie } from "cookies-next"
+import { Product } from "../lib/products"
+import { GetCartDocument, useAddToCartMutation } from "../types"
+import { ProductItem } from "./ProductItem"
