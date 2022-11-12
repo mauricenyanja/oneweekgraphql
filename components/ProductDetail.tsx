09b225ebe1b2a5e114064d55db1f3e05340dffec
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 01:10:49 +0300

    exported Productdetail

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -2,3 +2,8 @@ import { getCookie } from "cookies-next"
 import { Product } from "../lib/products"
 import { GetCartDocument, useAddToCartMutation } from "../types"
 import { ProductItem } from "./ProductItem"
+
+export function ProductDetail({ product }: { product:Product | null }) {
+    const cartId = String(getCookie("cartId"))
+    const
+}
