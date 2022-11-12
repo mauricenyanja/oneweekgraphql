bd91a813201478f5cb4f5b81b03c317e6144d4cf
Nyash-Mauro <mauricee423@gmail.com>
2022-11-12 23:51:32 +0300

    made a variable product page

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,7 +1,19 @@
 import { GetServerSideProps,InferGetServerSidePropsType,NextPage }
  from "next"
 
- import { Header } from "../../components/Header";
- import { ProductDetail } from "../../components/ProductDetail";
- import { Product, products } from "../../lib/products";
+ import { Header } from "../../components/Header"
+ import { ProductDetail } from "../../components/ProductDetail"
+ import { Product, products } from "../../lib/product"
  
+
+ const ProductPage: NextPage<
+ InferGetServerSidePropsType<typeof getServerSideProps>
+ > =  ({ product }) => (
+    <div>
+        <Header/>
+        <ProductDetail product={product}/>
+    </div>
+ )
+ export const getServerSideProps:GetServerSideProps<{
+    product:Product | null;
+ }> async ({ })
