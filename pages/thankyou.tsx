3a6c11d69829be2c0b16504a2f17ff78c9c3b2b8
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:19:51 +0300

    made variable thankyou

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -6,3 +6,7 @@ import { CartDetail } from "../components/CartDetail"
 import { Header } from "../components/Header"
 import { stripe } from "../lib/stripe"
 import { useGetCartQuery } from "../types"
+
+const ThankYou: NextPage<IProps> = ({ session }) => {
+    
+}
