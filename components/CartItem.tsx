45a2f7d7a9f4a628d0aae6b20f8509940fba0be0
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:50:00 +0300

    created decrease and remove from cart variable

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,26 +1,34 @@
-import Image from "next/image"
+import Image from "next/image";
 import {
   CartItem,
   GetCartDocument,
   useDecreaseCartItemMutation,
   useIncreaseCartItemMutation,
   useRemoveFromCartMutation,
-} from "../types"
-import { CloseIcon } from "./CloseIcon"
-import { MinusIcon } from "./MinusIcon"
-import { PlusIcon } from "./PlusIcon"
+} from "../types";
+import { CloseIcon } from "./CloseIcon";
+import { MinusIcon } from "./MinusIcon";
+import { PlusIcon } from "./PlusIcon";
 
 export function CartItem({
-    item,
-    cartId,
-    isReadOnly,
+  item,
+  cartId,
+  isReadOnly,
 }: {
-    item:CartItem,
-    cartId: string,
-    isReadOnly?: boolean
+  item: CartItem;
+  cartId: string;
+  isReadOnly?: boolean;
 }) {
-    const [increaseCartItem, { loading: increasingCartItem }] =
+  const [increaseCartItem, { loading: increasingCartItem }] =
     useIncreaseCartItemMutation({
       refetchQueries: [GetCartDocument],
-    })
+    });
+  const [decreaseCartItem, { loading: decreasingCartItem }] =
+    useDecreaseCartItemMutation({
+      refetchQueries: [GetCartDocument],
+    });
+  const [removeFromCart, { loading: removingFromCart }] =
+    useRemoveFromCartMutation({
+      refetchQueries: [GetCartDocument],
+    });
 }
