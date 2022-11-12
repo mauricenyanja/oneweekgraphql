b0e4abfaf22f87b13d03a3c363a21e71da6ef9bf
Nyash-Mauro <mauricee423@gmail.com>
2022-11-13 00:35:46 +0300

    xported CartDetail

--- components/CartDetail.tsx
+++ components/CartDetail.tsx
@@ -1,3 +1,12 @@
 import {CartFragment} from "../types"
 import { CartItem } from "./CartItem"
 
+export function CartDetail({
+    cart,
+    isReadOnly,
+}: {
+    cart: CartFragment | null | undefined;
+    isReadOnly?: boolean;
+}) {
+    ret
+}
